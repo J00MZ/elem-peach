41aa15c39ac25fc317bdb780061f7b5e99cdaef0
Joe Tavin <jtavin@gmail.com>
2021-07-05 20:11:08 +0300

    update README, add k8s yamls

--- README.md
+++ README.md
@@ -4,3 +4,39 @@ Peachy time summer is eh? 🍑🍑
 # Objective
 Present a solution for [Exercise](docs/exercise.pdf)
 
+## Building the App Docker image
+```
+cd <repo_directory>/rickandmorty
+docker build -t rickandmorty .
+```
+
+## Running the App locally
+```
+docker run -p 5000:5000 rickandmorty
+```
+## App Functionality
+1. To get all the live Humans from Earth
+Access the link **`http://localhost:5000/find-live-humans`** in a browser, or use CURL command
+```
+    curl http://localhost:5000/find-live-humans
+```
+
+2. To display the Live Humans from the csv file:  
+Access the link **`http://localhost:5000/csv`** in a browser, or use CURL command
+```
+    curl http://localhost:5000/csv
+```
+
+3. To check the application is healthy and running:  
+Access the link **`http://localhost:5000/healthcheck`** in a browser, or use CURL command
+```
+    curl http://localhost:5000/healthcheck
+```
+
+# Deploy App to Kubernetes
+```
+cd <REPOSITORY_ROOT>
+kubectl apply -f yamls
+kubectl port-forward service/rickandmorty 5000
+```
+Then you can access the application on localhost:5000
--- rickandmorty/app/app.py
+++ rickandmorty/app/app.py
@@ -1,6 +1,6 @@
 """Application main app."""
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, jsonify
 import requests
 import logging, coloredlogs
 import os, csv, tablib
@@ -9,7 +9,7 @@ app = Flask(__name__, static_url_path='')
 coloredlogs.install(level='DEBUG', isatty=True)
 logger = logging.getLogger('app_log')
 
-def print_as_csv(data):
+def print_to_csv(data):
         logger.debug(f'printing [{data}] results to CSV file')
         col_names = ['Name', 'Location', 'Image']    
         
@@ -19,9 +19,10 @@ def print_as_csv(data):
             tsv_output.writerows(data) 
 
 @app.route('/')
+@app.route('/healthcheck')
 def echo_isalive():
     service_name = 'Rick and Morty Service'
-    return f'{service_name} is Alive!'
+    return jsonify('{service_name} is Alive!')
 
 @app.route('/find-live-humans')
 def find_live_humans():
@@ -31,19 +32,24 @@ def find_live_humans():
     all_characters = response['results']
     logger.debug(all_characters)
     for char in all_characters:
-        if char['status'] == 'Alive' and char['species'] == 'Human' and char['location']['name'] == 'Earth':
+        if char['status'] == 'Alive' and char['species'] == 'Human' and 'Earth' in char['origin']['name']:
             char_details = [char['name'], char['location']['name'], char['image']]
             live_humans.append(char_details)
     logger.debug('Sending all Live Humans found on Earth to a CSV file!')
     logger.debug(f'Live Humans found: {live_humans}')
-    return(live_humans)
+    print_to_csv(live_humans)
+    if not live_humans:
+        ret_val = jsonify(results=0, message="No live Humans from Earth found! :-(")
+    else:
+        ret_val = jsonify(live_humans)
+    return(ret_val)
 
 @app.route('/csv')
-def show_csv_file(path):
+def show_csv_file():
     dataset = tablib.Dataset()
     with open(os.path.join(os.path.dirname(__file__), 'output.csv')) as f:
         dataset.csv = f.read()
-    return dataset.html
+    return jsonify(dataset.dict)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
